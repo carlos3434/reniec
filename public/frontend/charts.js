4dbc3f6e33458da544bf59483b5d145c50c09738
juan <carlos3434@hotmail.com>
2017-09-02 19:07:50 -0500

    vista de estadistica de suledos

--- public/frontend/charts.js
+++ public/frontend/charts.js
@@ -1,24 +1,77 @@
-
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
+var vm = new Vue({
+    el: '#portfolio',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        carreras: [],
+        carreraSelec: ''
+    }
+});
+
+var Empleabilidad = {
+    get: function get(careerId, callback) {
+        $.get("sueldos/" + careerId, function (response) {
+            callback(response);
+        }).done(function (response) {
+            //alert( "second success" );
+        }).fail(function (response) {
+            //alert( "error" );
+        }).always(function (response) {
+            //alert( "finished" );
+        });
+    }
+};
+
+var Carreras = {
+    get: function get() {
+        $.get("careers", function (response) {
+            vm.carreras = response.careers;
+        }).done(function (response) {
+            //alert( "second success" );
+        }).fail(function (response) {
+            //alert( "error" );
+        }).always(function (response) {
+            //alert( "finished" );
+        });
+    }
+};
+
+pintar = function pintar(sueldos) {
+    //console.log(sueldos);
+    var ctx = document.getElementById("myChart").getContext('2d');
+    //sueldos.data
+    //sueldos.labels
+
+    options = {
+        scales: {
+            xAxes: [{
+                gridLines: {
+                    offsetGridLines: true
+                }
+            }]
+        }
+    };
+    color = ["rgb(255, 99, 132)", "rgb(255, 159, 64)", "rgb(255, 205, 86)", "rgb(75, 192, 192)", "rgb(54, 162, 235)", "rgb(153, 102, 255)", "rgb(201, 203, 207)"];
+
+    data = {
+        labels: sueldos.labels,
         datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: sueldos.data,
             backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'],
             borderColor: ['rgba(255,99,132,1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'],
             borderWidth: 1
         }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
+    };
+
+    var myBarChart = new Chart(ctx, {
+        type: 'bar',
+        data: data,
+        options: options
+    });
+};
+$("#btn_sueldos").click(function () {
+    careerId = $('#slct_carrera').val();
+    Empleabilidad.get(careerId, pintar);
+});
+$(document).ready(function () {
+    Carreras.get();
 });
